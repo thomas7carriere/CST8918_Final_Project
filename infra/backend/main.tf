ce40e65aeed00e5613b195b6de8e20e70b504c3d
Thomas Carriere <thomas7carriere@gmail.com>
2026-08-13 20:21:36 -0400

    Format terraform files

--- infra/backend/main.tf
+++ infra/backend/main.tf
@@ -35,6 +35,6 @@ resource "azurerm_storage_container" "tfstate" {
   storage_account_id = azurerm_storage_account.tfstate.id
 }
 
-output "resource_group_name"  { value = azurerm_resource_group.tfstate.name }
+output "resource_group_name" { value = azurerm_resource_group.tfstate.name }
 output "storage_account_name" { value = azurerm_storage_account.tfstate.name }
-output "container_name"       { value = azurerm_storage_container.tfstate.name }
+output "container_name" { value = azurerm_storage_container.tfstate.name }
